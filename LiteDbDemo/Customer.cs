053b6b3f6f714d5b0aab9da07a226d555e5554f5
chenshengwei <dearbeans@163.com>
2022-06-23 15:02:22 +0800

    1

---
+++ LiteDbDemo/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiteDbDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Phones { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
