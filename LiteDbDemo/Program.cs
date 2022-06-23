f1bbdd5f73d94b9144bcb54beaca94afcf4683f2
chenshengwei <dearbeans@163.com>
2022-06-23 14:55:12 +0800

    初始项目

---
+++ LiteDbDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiteDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
